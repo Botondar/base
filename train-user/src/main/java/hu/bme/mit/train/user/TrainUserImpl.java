749410897c5ddb57480fb11240bd3cc9f305e353
botondar <m.botond7@gmail.com>
2022-05-09 14:58:17 +0200

    Added timer to TrainUserImpl

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -3,13 +3,26 @@ package hu.bme.mit.train.user;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 
+import java.util.Timer;
+import java.util.TimerTask; 
+
 public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private Timer timer;
+
+	class TimerFunction extends TimerTask {
+		@Override
+		public void run() {
+			TrainUserImpl.this.controller.followSpeed(); 
+		} 
+	} 
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
+		timer = new Timer(); 
+		timer.scheduleAtFixedRate(new TrainUserImpl.TimerFunction(), 0, 500); 
 	}
 
 	@Override
